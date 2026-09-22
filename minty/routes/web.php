4dc68442fd6dd01b7fb64e4be8eb004fb8124537
Mateo <mateo@etxea.it>
2026-09-22 12:10:56 +0200

    Ajout des routes pour les controllers

--- minty/routes/web.php
+++ minty/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\CarteController;
+use App\Http\Controllers\CarteUtilisateurController;
+use App\Http\Controllers\ExtensionController;
+use App\Http\Controllers\JeuController;
+use App\Http\Controllers\TypeSwuController;
+use App\Http\Controllers\UtilisateurController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('jeux', JeuController::class)->parameters([
+    'jeux' => 'jeu',
+]);
+Route::resource('extensions', ExtensionController::class);
+Route::resource('utilisateurs', UtilisateurController::class);
+Route::resource('cartes', CarteController::class);
+Route::resource('type-swu', TypeSwuController::class)->parameters([
+    'type-swu' => 'typeSwu',
+]);
+Route::resource('carte-utilisateur', CarteUtilisateurController::class)->parameters([
+    'carte-utilisateur' => 'carteUtilisateur',
+]);
